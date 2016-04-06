8d5b6fcad4c947c519372bf4e21fa547425d1645
Henry Yu <manifestduels@gmail.com>
2016-04-06 14:04:14 -0400

    its good

--- src/vector_space_models/vsm.py
+++ src/vector_space_models/vsm.py
@@ -3,6 +3,8 @@ import os
 import sys
 import glob
 import math
+import operator
+from random import randint
 from collections import Counter
 
 def generateStopWords(tf, df, threshold):
@@ -45,14 +47,153 @@ def getEuclideanDistance(d1, d2):
 
     return math.sqrt(sum)
 
+def getCosineDistance(d1, d2):
+    sum = 0
+    product = 0
+    i = 0
+    j = 0
+    for (k, v) in d1.items():
+        if k in d2:
+            sum += (v * d2[k])
+            i += v ** 2
+            j += d2[k] ** 2
+        # else:
+        #     #if it doesnt exist??
+        #     sum += v ** 2
+
+    for (k, v) in d2.items():
+        if k not in d1: j += v ** 2
+
+    print sum
+    print i
+    print j
+    if sum == 0 or i == 0 or j == 0: return 100000
+    return float(sum)/(float(math.sqrt(i))*float(math.sqrt(j)))
+
+# def initCentroids(docs, k, T):
+#     #centroids
+#     i, count =0
+#     D = docs.size()
+#     a = {}
+#     centroid =[]
+#
+#     while(a.size() < k):
+#         i = randint(0,D)
+#         if(not a.contains(i))
+#
+# def addtodict(dic, k):
+#     dic ={}
+#     if(dic.__contains__(k)):
+#         dic[k]+=1
+#
+#     else:
+#         dic[k]=0
+#
+#     return dic
+#
 
 
+Tcorrect = 0
 
 IN_DIR = sys.argv[1]
 filenames = glob.glob(os.path.join(IN_DIR,'*'))
 tf = [getTermFrequencies(open(filename)) for filename in filenames]
+
+
+
+IN_DIR = sys.argv[2]
+filenamesdev = glob.glob(os.path.join(IN_DIR,'*'))
+tf1 = [getTermFrequencies(open(filename)) for filename in filenamesdev]
+df1 = getDocumentFrequencies(tf1)
+Tdoc = filenamesdev.__len__()
+
+
+
+#
+# for term in tf:
+#     print term
+    #print "\n"
 df = getDocumentFrequencies(tf)
+#print df
+# for docterm in df:
+#     print docterm
 sw = generateStopWords(tf, df, 200)
 sw = generateStopWords(tf, df, .8)
+#dev data---------
+tfidf1 = getTFIDFs(tf1, df1, sw)
 tfidf = getTFIDFs(tf, df, sw)
-getEuclideanDistance(tfidf[0], tfidf[1])
+
+knn = 40
+trainnum = 0
+
+for dev in tfidf1:
+    k = { }
+    print filenamesdev[trainnum].split("/")[9]
+    correct = filenamesdev[trainnum].split("/")[9].split("_")[0]
+    print "votes: "
+    j = 0
+    minv = 10000
+    for train in tfidf:
+        #dist = getEuclideanDistance(train, dev)
+        dist = getCosineDistance(train, dev)
+
+        #print dist
+
+        if k:
+            #x = j-2
+            if k.__len__() >= knn:
+                greatest = max(k.iteritems(), key=operator.itemgetter(1))[0]
+                # print "this is greatest: "
+                # print greatest
+                if dist < minv: minv = dist
+                if k.get(greatest) > dist:
+                    del k[greatest]
+                    k[j]=dist
+                    #print dist
+            else:   k[j]=dist
+
+        else:
+            k = {j:dist}
+
+        #print j
+        #print k.get(j)
+        #print k
+
+        j+=1
+#prints the name of dev train and distance
+    for key in k:
+        #print filenames[key]
+        genre = filenames[key].split("/")[9].split("_")[0]
+        print genre
+        #print k[key]
+        dic = {}
+        if (dic.__contains__(genre)):
+            dic[genre] += 1
+
+        else:
+            dic[genre] = 0
+
+        Mgenre = max(dic.iteritems(), key=operator.itemgetter(1))[0]
+
+
+    #print minv
+    # print "answer:  "
+    # print Mgenre
+    # print "done"
+
+    if(correct == Mgenre):
+        Tcorrect +=1
+    #sys.exit(0)
+    trainnum += 1
+    # if len(k) -1 > 3:
+    #     for i in range(len(k) - 3, len(k)):
+    #         print k
+
+print "correct matches: " + Tcorrect.__str__()
+print Tdoc
+perofcorr = (float(Tcorrect) / Tdoc)
+print perofcorr
+
+print "percentage: " + perofcorr.__str__()
+# for i in range(len(k)-3,len(k)):
+#     print k[i]
